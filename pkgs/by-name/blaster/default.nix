18cafdb08aa45de67f4d54d36052b9a6c2bc5adc
floyd <floyd.zweydinger@rub.de>
2025-09-02 10:08:08 +0200

    gitbak

--- pkgs/by-name/blaster/default.nix
+++ pkgs/by-name/blaster/default.nix
@@ -1,35 +1,41 @@
-{ pkgs, lib, stdenv, fetchgit }:
-stdenv.mkDerivation {
+{ pkgs, lib, stdenv, fetchFromGitHub, python3Packages }:
+
+python3Packages.buildPythonApplication {
   pname = "blaster";
   version = "0.0.1";
 
-  src = fetchgit {
-    url = "https://github.com/ludopulles/BLASter";
+  src = fetchFromGitHub {
+    owner = "ludopulles";
+    repo = "BLASter";
     rev = "c6b9328bcf4ab6709d8f94f1bdd9e4b5491cd082";
-    sha256 = "sha256-quyJSl9eLEQXjDPAFTisZqd3HvqXH/6Z7Uh1bukSM0E=";
+    hash = "sha256-quyJSl9eLEQXjDPAFTisZqd3HvqXH/6Z7Uh1bukSM0E=";
   };
 
   nativeBuildInputs = with pkgs; [
-    fplll
-    python3
-  ];
+    pkg-config
+  ] ++ (with python3Packages; [
+    cython
+    setuptools
+  ]);
 
   buildInputs = with pkgs; [
-    cmake
-    clang
-    gbenchmark
-    gmp
-    mpfr
     eigen
     openblas
-    pkg-config
+    gmp
+    mpfr
   ];
 
-  #installPhase = ''
-  #'';
-  
+  propagatedBuildInputs = with python3Packages; [
+    numpy
+    cysignals
+  ];
+
+  preBuild = ''
+    ln -sf ${pkgs.eigen}/include/eigen3 eigen3
+  '';
+
   meta = with lib; {
-    description = " Fast lattice reduction using segmentation, multithreading, Seysen reduction and BLAS ";
+    description = "Fast lattice reduction using segmentation, multithreading, Seysen reduction and BLAS";
     homepage = "https://github.com/ludopulles/BLASter";
     license = licenses.mit;
     platforms = platforms.linux;
