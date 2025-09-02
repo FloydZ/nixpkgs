bad61b2f8db70686653edd4fdc67b12566a810c6
floyd <floyd.zweydinger@rub.de>
2025-09-02 19:03:54 +0200

    updated list of packages

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@ let
   #   rev = "9ccf8be";
   #   sha256 = "sha256-/EXYeJ1PCqa7Y8QhJiM5/4+9Sk378Qt95xit5/pwCng=";
   # }) { inherit pkgs; };
-  flake = builtins.getFlake "github:FloydZ/nixpkgs/9ccf8be";
+  flake = builtins.getFlake "github:FloydZ/nixpkgs/5a524f8";
   myPkgs = flake.packages.${system};
 in
 stdenv.mkDerivation {
@@ -21,8 +21,6 @@ stdenv.mkDerivation {
     
     # Your custom packages from this repo
     myPkgs.blaster
-    myPkgs.nanobench
-    myPkgs.shot
     
     # Python environment (since blaster is a Python app)
     python3
@@ -34,8 +32,6 @@ stdenv.mkDerivation {
     echo "Development shell loaded!"
     echo "Available custom packages:"
     echo "  - blaster: Fast lattice reduction using segmentation and BLAS"
-    echo "  - nanobench: Benchmarking library"
-    echo "  - shot: Screenshot tool"
     echo ""
     echo "Run 'blaster --help' to get started with BLASter"
   '';
